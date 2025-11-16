2b79db03c8aa1e5f1a462866c42c05dcc155582d
Vivek <vivek000@gmail.com>
2025-11-16 15:55:11 +0530

    Fix MongoDB connection string parsing for database name

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -14,7 +14,16 @@ const connectDB = async () => {
             console.log("MongoDB Disconnected");
         });
 
-        await mongoose.connect(`${process.env.MONGODB_URI}/imagify`);
+        // Parse the connection string to insert database name correctly
+        const mongoUri = process.env.MONGODB_URI;
+        const dbName = 'imagify';
+        
+        // If URI already has query params, insert db name before them
+        const finalUri = mongoUri.includes('?') 
+            ? mongoUri.replace('mongodb.net/', `mongodb.net/${dbName}`)
+            : `${mongoUri}/${dbName}`;
+            
+        await mongoose.connect(finalUri);
 
         console.log("MongoDB Connection Attempted");
     } catch (error) {
